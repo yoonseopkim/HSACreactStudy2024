23f4c8214d4f8b1c71d40015d0db4d04481e1eb2
Mgby1201sh <ajin1991@naver.com>
2024-04-04 17:45:26 +0900

    커밋메세지

--- src/App.js
+++ src/App.js
@@ -1,48 +1,82 @@
-/* eslint-disable */
-
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import logo from './logo.svg'; 
+import './App.css'; // 앱의 CSS 스타일을 불러옵니다.
+import { useState } from "react"; 
 
 function App() {
+    let [title, setTitle] = useState(0);
+    let [글제목, 글제목변경] = useState(['코트', '후드티', '신발']);
+    let [좋아요개수, 좋아요변경] = useState([0,0,0]); // 각 글의 좋아요 개수를 저장하는 변수와 좋아요 개수를 변경하는 함수를 선언
 
-  let [나, 변경] = useState(['나이', '취미', '알바']);
-  let [따봉, 따봉변경] = useState(0);
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <h4>{post}</h4>
-      </div>
-      <button onClick={()=>{글제목변경(['23', '여행', '카페 알바']);}}>?</button>
+    let [modal, setModal] = useState(false); // 모달의 상태를 저장하는 변수와 모달의 상태를 변경하는 함수를 선언
+    let [counter, setCounter] = useState(0); // 카운터를 저장하는 변수와 카운터를 변경하는 함수를 선언
+    let [입력값, 입력값변경] = useState(''); // 입력값을 저장하는 변수와 입력값을 변경하는 함수를 선언
 
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <div style={{fontSize: '60px', color: 'gray'}}>REACT</div>
+            </div>
+            <input onChange={(e)=>{
+              // 입력값을 변경하는 함수를 호출하여 입력값을 업데이트
+              입력값변경(e.target.value)
+            }} type="text"/>
+            <button onClick={()=>{
+                // 입력값을 글 목록에 추가
+                let copy = [...글제목];
+                copy.unshift(입력값); // 글제목 앞에 입력값(요소)이 추가된다
+                글제목변경(copy);
+            }}>글발행</button>
 
-      <div className="list">
-        <h4>{ 나[0] } <span onClick={ () => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{ 나[1] }</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{ 나[2] }</h4>
-        <p>2월 18일 발행</p>
-      </div>
+            {/* 글 목록 */}
+            {글제목.map(function (value, i) {
+                return (
+                    <div className="list" key={i}>
 
-      <Sang></Sang>
-    </div>
-  );
+                        <h4 onClick={() => {
+                            // 모달을 열고 선택된 글의 인덱스를 업데이트
+                            { setModal(true); setTitle(i); } // () 안 true 는 모달 상태를 활성화 시킴
+                            // 카운터를 증가시키고 모달을 염
+                            setCounter(counter + 1);
+                            setModal(true);
+                        }}>
+                            {value}
+                            <button onClick={()=>{
+                                let copy = [...글제목];
+                                copy.splice(i,1); // i번째 인덱스부터 1개의 요소를 제거한다.
+                                글제목변경(copy);
+                            }}>delete</button>
+                            <button onClick={() => {
+                                let copy = [...글제목];
+                                copy.sort(); // 요소를 적절한 위치에 따라 정렬하는 매서드. 숫자에 sort()를 쓰면 작은 숫자부터 큰 숫자 순서대로 정렬된다.
+                                글제목변경(copy);
+                            }}>가나다순정렬
+                            </button>
+                            <span  onClick={() => {
+                                // 선택된 글의 좋아요 개수를 증가시킴
+                                let copy = [...좋아요개수];
+                                copy[i] = copy[i] + 1; //copy 배열의 인덱스 i에 해당하는 요소를 1씩 증가시킨다.
+                                좋아요변경(copy)
+                            }}>👍</span> {좋아요개수[i]} </h4>
+                        <p>2월 17일 발행</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
-function Sang() {
-  return (
-    <div className='Sang'>
-    <h4>윤상혁입니다</h4>
-    <p>23살 입니다.</p>
-    <p>23학번이고, 여행 다니는 것을 좋아합니다. 카페 알바를 하고 있습니다.</p>
-  </div>
-  )
+// 모달 컴포넌트
+function Modal(props) {
+    return (
+        <div className="modal">
+            {/* 선택된 글의 제목을 표시합니다. */}
+            <h4>{props.글제목[props.title]}</h4>
+            {/* 수정 버튼 */}
+            <button>변경</button>
+            <p>날짜</p>
+            <p>상세내용</p>
+        </div>
+    )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,20 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+function Sang1() {
+  const Home = () => <h1>Home Page</h1>
+  const About = () => <h1>About Page</h1>
+  const Contact = () => <h1>Contact Page</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>/
+    </BrowserRouter>
+  );
+}
+
+export default Sang1;
